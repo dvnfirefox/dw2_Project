fe8102ed996b9fd3b11fc571b64ca567809efe6c
dvnfirefox <134988327+dvnfirefox@users.noreply.github.com>
2024-03-07 12:34:46 -0500

    select button sequence

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 const color = ['#AD1D45','#7B6CF6','#ECBC55', 'black'];
 const tokenId = ['random1', 'random2', 'random3'];
 const textId = ['randomText1', 'randomText2', 'randomText3'];
-const hiddenColor = [];
-var colorcount;
+var hiddenColor = [];
+const tokenButton = ['redText', 'blueText', 'yellowText'];
+var colorcount = 0;
 
 function randomColor() {
     return Math.floor(Math.random() *3);
@@ -19,7 +20,6 @@ function tokencolorIni(){
     do {
         hiddenColor[2] = randomColor();
     }while (hiddenColor[0] === hiddenColor[2]||hiddenColor[1] === hiddenColor[2])
-    resetBoard();
 }
 function showAllColor(){
     for(let i = 0; i<3; i++) {
@@ -31,21 +31,44 @@ function resetBoard(){
     for(let i = 0; i<3; i++) {
         colorSet(tokenId[i], 3)
         document.getElementById(textId[i]).innerHTML = "?";
+        document.getElementById(tokenButton[i]).innerHTML = "";
     }
+    colorcount = 0;
     tokencolorIni();
 }
-function colorSelect(color){
 
+function selectRed(){
+    if(colorcount > 2){
 
+    }else {
+        document.getElementById('redText').innerHTML = (colorcount + 1);
+        colorcount++;
+    }
+}
+function selectblue(){
+    if(colorcount > 2){
+
+    }else {
+        document.getElementById('blueText').innerHTML = (colorcount + 1);
+        colorcount++;
+    }
+}
+function selectyellow(){
+    if(colorcount > 2){
+
+    }else {
+        document.getElementById('yellowText').innerHTML = (colorcount + 1);
+        colorcount++;
+    }
 }
 document.getElementById('resetButton').addEventListener("click", resetBoard, false);
 document.getElementById('checkButton').addEventListener("click", showAllColor, false);
-document.getElementById("red").addEventListener("click", colorSelect(0), false);
-document.getElementById("blue").addEventListener("click", colorSelect(1), false);
-document.getElementById("yellow").addEventListener("click", colorSelect(2), false);
-
+document.getElementById("red").addEventListener("click", selectRed, false);
+document.getElementById("blue").addEventListener("click", selectblue, false);
+document.getElementById("yellow").addEventListener("click", selectyellow, false);
 
 resetBoard();
+
 
 
 
