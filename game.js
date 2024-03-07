e88cd1dee151117746e1cbf7e79cfad16b2817db
dvnfirefox <134988327+dvnfirefox@users.noreply.github.com>
2024-03-07 09:53:06 -0500

    add button for check and reset

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
-var color = ['#AD1D45','#7B6CF6','#ECBC55'];
-var tokenId = ['random1', 'random2', 'random3'];
-var hiddenColor = [];
+const color = ['#AD1D45','#7B6CF6','#ECBC55', 'black'];
+const tokenId = ['random1', 'random2', 'random3'];
+const textId = ['randomText1', 'randomText2', 'randomText3'];
+const hiddenColor = [];
 
 function randomColor() {
-    var random = Math.floor(Math.random() *3);
-    return random;
+    return Math.floor(Math.random() *3);
+
 }
 function colorSet(id, colorNumber) {
     document.getElementById(id).style.backgroundColor = color[colorNumber];
@@ -17,16 +18,29 @@ function tokencolorIni(){
     do {
         hiddenColor[2] = randomColor();
     }while (hiddenColor[0] === hiddenColor[2]||hiddenColor[1] === hiddenColor[2])
-    showAllColor()
+    resetBoard();
 }
 function showAllColor(){
     for(let i = 0; i<3; i++) {
-        colorSet(tokenId[i], hiddenColor[i])
+        colorSet(tokenId[i], hiddenColor[i]);
+        document.getElementById(textId[i]).innerHTML = "";
     }
 }
+function resetBoard(){
+    for(let i = 0; i<3; i++) {
+        colorSet(tokenId[i], 3)
+        document.getElementById(textId[i]).innerHTML = "?";
+    }
+    tokencolorIni();
+
+
+}
+function createEventListener(){
+    document.getElementById('resetButton').addEventListener("click", resetBoard, false);
+    document.getElementById('checkButton').addEventListener("click", showAllColor, false);
+}
+createEventListener();
+resetBoard();
 
 
 
-
-
-tokencolorIni();
